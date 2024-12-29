414a564423d4d63606d5a07d375e5c80f5370afa
misung.dev <misung.dev@gmail.com>
2024-12-30 08:38:37 +0900

    feat: 유저 상태 정보 확인

--- Models/user.js
+++ Models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Schema? 데이터의 설계도와 같음. 내가 받을 데이터가 이렇게 생겼다라는 것을 설명
-// 유저이름, token(이름, 연결 id 정보), online(온/오프라인의 상태를 보여줌)
+// 유저이름, token(이름, 연결 id 정보), online(온/오프라인의 상태를 보여줌) => 유저정보
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
--- utils/io.js
+++ utils/io.js
@@ -1,7 +1,22 @@
+const userController = require("../Controllers/user.controller");
+
 // io 관련 모든 작업
 module.exports = function (io) {
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      // 로그인을 들었을때 하고 싶은 말
+      console.log("backend", userName);
+      // 여기서의 로그인: 받은 유저정보를 저장하고 소켓 아이디 정보도 저장
+      // 유저정보를 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("user is disconnected");
